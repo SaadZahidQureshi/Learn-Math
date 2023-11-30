3009be1cd1ea592e0c5cc502975ec46dfd81fce6
SaadZahidQureshi <saadzahid133204@gmail.com>
2023-12-01 01:17:00 +0500

    responsiveness on login, signup, email verify page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-function hideYoutube(){
-    document.getElementById('youtube-video-container').style.display = 'none';
+function showYoutube(){
+    document.getElementById('youtube-video-container').classList.remove('d-none');
+}
+
+function showError(){
+  document.getElementById('video-container').classList.remove('d-none');
+  document.getElementById('wrong-message-box').classList.remove('d-none')
+  document.getElementById('wrong-answer').classList.add('option-selected-wrong')
+  console.log(document.getElementById('wrong-answer'))
 }
