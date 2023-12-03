6b9d18cdad0b1972748a6139a006f7693ffb403c
SaadZahidQureshi <saadzahid133204@gmail.com>
2023-12-04 00:14:37 +0500

    responsiveness added on some pages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function showYoutube(){
-    document.getElementById('youtube-video-container').classList.remove('d-none');
+    document.getElementById('video-container').classList.remove('d-none');
 }
 
 function showError(){
   document.getElementById('video-container').classList.remove('d-none');
   document.getElementById('wrong-message-box').classList.remove('d-none')
   document.getElementById('wrong-answer').classList.add('option-selected-wrong')
-  console.log(document.getElementById('wrong-answer'))
+  // document.getElementsByClassName('justify-center-sm')
 }
