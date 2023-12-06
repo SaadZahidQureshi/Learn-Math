e2c9ac7bcdb79268b9dc8aba2afffbd2a62df7df
SaadZahidQureshi <saadzahid133204@gmail.com>
2023-12-06 18:07:47 +0500

    some modifications in design and js

--- user/js/slider.js
+++ user/js/slider.js
@@ -3,24 +3,37 @@ const sliderList = document.getElementById('slider-list');
 // Sample data for slider items
 const sliderData = [
   { text: 'Level 1', name: 'level-1', href: '../html/question.html'+'?id=level-1' },
-  { text: 'Level 2',  name: 'level-2', href: '../html/Question-with-image.html'+'?id=level-2' },
-  { text: 'Level 3',  name: 'level-3', href: '../html/Question-wrong-ans.html'+'?id=level-3' },
-  { text: 'Level 4',  name: 'level-4', href: '#' },
-  { text: 'Level 5',  name: 'level-5', href: '#' },
-  { text: 'Level 6',  name: 'level-6', href: '#' },
-  { text: 'Level 7',  name: 'level-7', href: '#' },
-  { text: 'Level 8',  name: 'level-8', href: '#' },
-  { text: 'Level 9',  name: 'level-9', href: '#' },
-  { text: 'Level 10', name: 'level-10',  href: '#' },
-  { text: 'Level 11', name: 'level-11',  href: '#' },
-  { text: 'Level 12', name: 'level-12',  href: '#' },
-  { text: 'Level 13', name: 'level-13',  href: '#' },
-  { text: 'Level 14', name: 'level-14',  href: '#' },
-  { text: 'Level 15', name: 'level-15',  href: '#' },
-  { text: 'Level 16', name: 'level-16',  href: '#' },
+  { text: 'Level 2', name: 'level-2', href: '../html/Question-with-image.html'+'?id=level-2' },
+  { text: 'Level 3', name: 'level-3', href: '../html/Question-wrong-ans.html'+'?id=level-3' },
+  { text: 'Level 4', name: 'level-4', href: '../html/question.html'+'?id=level-4' },
+  { text: 'Level 5', name: 'level-5', href: '../html/Question-with-image.html'+'?id=level-5' },
+  { text: 'Level 6', name: 'level-6', href: '../html/Question-wrong-ans.html'+'?id=level-6' },
+  { text: 'Level 7', name: 'level-7', href: '../html/question.html'+'?id=level-7' },
+  { text: 'Level 8', name: 'level-8', href: '../html/Question-with-image.html'+'?id=level-8' },
+  { text: 'Level 9', name: 'level-9', href: '../html/Question-wrong-ans.html'+'?id=level-9' },
+  { text: 'Level 10', name: 'level-10', href: '../html/question.html'+'?id=level-10' },
+  { text: 'Level 11', name: 'level-11', href: '../html/Question-with-image.html'+'?id=level-11' },
+  { text: 'Level 12', name: 'level-12', href: '../html/Question-wrong-ans.html'+'?id=level-12' },
+  { text: 'Level 13', name: 'level-13', href: '../html/question.html'+'?id=level-13' },
+  { text: 'Level 14', name: 'level-14', href: '../html/Question-with-image.html'+'?id=level-14' },
+  { text: 'Level 15', name: 'level-15', href: '../html/Question-wrong-ans.html'+'?id=level-15' },
+  { text: 'Level 16', name: 'level-16', href: '../html/question.html'+'?id=level-16' },
   // Add more data as needed
 ];
 
+// window.addEventListener('load', function(){
+//   sliderList.innerHTML = ''; // Clear existing items
+
+//   sliderData.forEach((item, index) => {
+//       const listItem = document.createElement('a');
+//       listItem.className = 'slider-items text-center text-decoration-none';
+//       listItem.innerText = item.text;
+//       listItem.href = item.href;
+//       listItem.dataset.name = item.name;
+
+//       sliderList.appendChild(listItem);
+//   });
+// })
 // Dynamically generate slider items
 function generateSliderItems() {
   sliderList.innerHTML = ''; // Clear existing items
@@ -35,23 +48,23 @@ function generateSliderItems() {
       sliderList.appendChild(listItem);
   });
 
-  // updateSlider(); // Update slider position after generating items
 }
 
-// Initial setup
 generateSliderItems();
 
 const sliderItems = document.querySelectorAll('.slider-items');
 const totalItems = sliderItems.length;
 let currentIndex = 0;
-let itemsDisplayed = 10;
+let itemsDisplayed = calculateItemsDisplayed();
 const itemWidth = sliderItems[0].offsetWidth;
 
+function calculateItemsDisplayed() {
+  // Adjust this function based on your desired logic for different viewport widths
+  return window.innerWidth < 768 ? 4 : 10; // Example: 4 items on mobile, 10 items on desktop
+}
 
 function nextSlide() {
   currentIndex = (currentIndex + 1) % totalItems; //first time it (0+1)%16 = 1
-
-
   if ( currentIndex == sliderItems.length+1 - itemsDisplayed ){
     currentIndex =0;
   }
@@ -61,19 +74,26 @@ function nextSlide() {
 
 function prevSlide() {
   currentIndex = (currentIndex - 1 + totalItems) % totalItems; // (0-1+16) = 15%16 = 15
-
   if (currentIndex == 15) {
     currentIndex = sliderItems.length-itemsDisplayed;
   }
-
-  console.log(currentIndex)
   updateSlider();
 }
 
 function updateSlider() {
-  const newPosition = -currentIndex * itemWidth;//
-  sliderList.style.transform = `translateX(${newPosition}px)`;
-  // console.log(itemWidth)
+
+  // if(this.window.innerWidth < 768 && currentIndex <= 12){
+  //   const newPosition = -currentIndex * itemWidth;//
+  //   sliderList.style.transform = `translateX(${newPosition}px)`;
+  // }
+  // if(this.window.innerHeight > 768 && currentIndex <=7){
+  //   const newPosition = -currentIndex * itemWidth;//
+  //   sliderList.style.transform = `translateX(${newPosition}px)`;
+  // }
+
+    const newPosition = -currentIndex * itemWidth;//
+    sliderList.style.transform = `translateX(${newPosition}px)`;
+    
 }
 
 
@@ -86,10 +106,18 @@ window.addEventListener('load', function() {
   if(uni_id[uni_id.length-1] == '#'){
     uni_id = uni_id.slice(0,-1)
   }
-
+  let selectedIndex = -1
   const items = document.querySelectorAll('.slider-items');
-  items.forEach(item =>{
+  items.forEach((item,index) =>{
     if(item.getAttribute('data-name') == uni_id){
+      selectedIndex = index
+      currentIndex = selectedIndex
+      console.log(selectedIndex)
+      // if (currentIndex <= 7) {
+      //   currentIndex = sliderItems.length-itemsDisplayed;
+      // }
+      updateSlider()
+      
       items.forEach(item =>{
         item.classList.remove('slider-active')
       })
@@ -98,4 +126,5 @@ window.addEventListener('load', function() {
   })
 
 })
+
 
