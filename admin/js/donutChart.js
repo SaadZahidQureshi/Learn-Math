e4447e84375f1ed2b9b3702930583467591cfbcc
SaadZahidQureshi <saadzahid133204@gmail.com>
2023-12-13 18:50:02 +0500

    multiple admin dashborad pages added just overview designed yet

--- admin/js/donutChart.js
+++ admin/js/donutChart.js
@@ -3,8 +3,9 @@ const values2 = [50, 50];
 const color = ["#F14336", "#28B446"];
 total = values2.reduce((accumulator, currentValue) => accumulator + currentValue)
 label = values2.map(value => Math.round((value / total) * 100) +'%');
-label[0]= label[0]+' Correct Ans'
-label[1]= label[1]+' Wrong Ans'
+label[0]= label[0]+' Wrong Ans'
+label[1]= label[1]+' Correct Ans'
+
 
 // Doughnut chart data
 const doughnutData = {
@@ -18,6 +19,8 @@ const doughnutData = {
 ],
 };
 
+
+//show text in center of doughnut chart
 const textCenter ={
   id: 'textCenter',
   afterDatasetsDraw(chart, args, plugins){
@@ -38,9 +41,16 @@ const doughnutConfig = {
     // cutoutPercentage: 40,
     maintainAspectRatio: false,
     plugins:{
+      layout:{
+        padding:{
+          top:50
+        }
+      },
       legend:{
         display:true,
+        reverse: true,
         position: 'bottom',
+        
 
         labels:{
         }
