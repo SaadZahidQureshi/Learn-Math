7470c5bb754b0d112fd42c587f6f3100add7b66c
SaadZahidQureshi <saadzahid133204@gmail.com>
2023-12-14 19:31:04 +0500

    admin pannel user and category pages added

--- admin/js/common.js
+++ admin/js/common.js
@@ -9,3 +9,25 @@ function toggleActive(element) {
     // Add 'active' class to the clicked nav link
     element.classList.add('active');
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Function to get query parameters from the URL
+  function getQueryParam(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  }
+
+  // Check if the showAlert parameter is present in the URL
+  const showAlertParam = getQueryParam("showAlert");
+//   console.log(showAlertParam)
+  if (showAlertParam === "true") {
+    // The showAlert parameter is present, display the alert
+    const alertBox = document.querySelector('.alert.alert-success');
+    if (alertBox) {
+      alertBox.classList.remove('d-none');
+    }
+  }
+});
+
